799d6c0036651d7eba1e3bb1ec00bf06216e374a
Naimcoder <isteiaknaim12@gmail.com>
2022-11-03 16:42:36 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require('express')
 const cors= require ('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express();
 const port= process.env.PORT || 5000;
 
@@ -23,12 +23,63 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run (){
     try{
         const serviceCollection= client.db('genis-car').collection('services')
+        const orderCollection= client.db('genis-car').collection('orders')
+
         app.get('/services',async(req,res)=>{
          const query={}
          const cursor= serviceCollection.find(query)
          const services= await cursor.toArray()
          res.send(services)
         })
+       
+    app.get('/services/:id',async(req,res)=>{
+     const id = req.params.id
+     const query ={_id:ObjectId(id)}
+     const service= await serviceCollection.findOne(query)
+     res.send(service)
+    })
+
+  app.get('/orders',async(req,res)=>{
+    let query={}
+    if (req.query.email) {
+        query={
+            email:req.query.email
+        }
+    }else{
+        query= {}
+    }
+    const cursor= orderCollection.find(query)
+    const result= await cursor.toArray()
+    res.send(result)
+  })
+   
+
+    app.post('/orders',async(req,res)=>{
+        const order= req.body;
+        const result= await orderCollection.insertOne(order)
+        res.send(result)
+    })
+
+       app.patch('/orders/:id',async(req,res)=>{
+    const id= req.params.id;
+    const status= req.body.status
+    const fillter={_id:ObjectId(id)}
+    const options= {upsert: true}
+    const updateDoc = {
+        $set:{
+         status: status,
+        }
+    }
+    const result= await orderCollection.updateOne(fillter,updateDoc,options)
+    res.send(result)
+   })
+
+    app.delete('/orders/:id',async(req,res)=>{
+        const id= req.params.id
+        const query= {_id:ObjectId(id)}
+        const result= await orderCollection.deleteOne(query)
+        res.send(result)
+    })
 
     }
     finally{
@@ -36,7 +87,6 @@ async function run (){
     }
 }
 run().catch (error=>console.log(error))
-
 
 
 
